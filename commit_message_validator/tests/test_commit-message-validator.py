9109bd158a3befdc3257946ee0c826b44c00facb
Kunal Mehta <legoktm@member.fsf.org>
2019-07-26 00:24:42 -0400

    Use pytest.mark.parametrize instead of a metaclass for generating test cases
    Much cleaner \o/
    
    Change-Id: I08bb6a511c1e4f3d529b00da2f5e4865054a4ab2

--- commit_message_validator/tests/test_commit-message-validator.py
+++ commit_message_validator/tests/test_commit-message-validator.py
@@ -2,8 +2,9 @@
 
 import os
 import re
-from six import with_metaclass
 import sys
+
+import pytest
 
 import commit_message_validator as cmv
 
@@ -24,9 +25,12 @@ MESSAGE_VALIDATOR_MAP = {
     'GerritMessageValidator': GerritMessageValidator,
     'GitHubMessageValidator': GitHubMessageValidator,
 }
+# Regular expression for matching ANSI escape sequences
+# https://stackoverflow.com/a/14693789/8171
+RE_ESC = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 
 
-class MetaValidator(type):
+def generate_tests():
     """
     Create test class which checks the output for a message.
 
@@ -37,53 +41,43 @@ class MetaValidator(type):
 
     Filenames for tests that will pass validation must end with 'ok'.
     """
+    base_path = os.path.join(
+        os.path.dirname(__file__), 'data')
+    for message_validator_name in os.listdir(base_path):
+        specific_message_validator_test_path = os.path.join(
+            base_path, message_validator_name)
 
-    def __new__(cls, name, bases, dct):
-        # Regular expression for matching ANSI escape sequences
-        # https://stackoverflow.com/a/14693789/8171
-        re_esc = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
-
-        def create_test_method(msg, expected, expected_exit_code,
-                               message_validator_name):
-            def test(self):
-                saved_stdout = sys.stdout
-                self.maxDiff = None
-                try:
-                    out = StringIO()
-                    sys.stdout = out
-                    exit_code = cmv.check_message(
-                        msg.splitlines(),
-                        MESSAGE_VALIDATOR_MAP[message_validator_name])
-                    # Ignore ANSI escapes in output
-                    plain_out = re_esc.sub('', out.getvalue())
-                    assert expected == plain_out
-                    assert expected_exit_code == exit_code
-                finally:
-                    sys.stdout = saved_stdout
-            return test
-
-        base_path = os.path.join(
-            os.path.dirname(__file__), 'data')
-        for message_validator_name in os.listdir(base_path):
-            specific_message_validator_test_path = os.path.join(
-                base_path, message_validator_name)
-
-            for fn in os.listdir(specific_message_validator_test_path):
-                test, _, extension = fn.rpartition('.')
-                fn = os.path.join(specific_message_validator_test_path, test)
-                if extension == 'msg' and os.path.isfile(fn + '.out'):
-                    exit_code = 0 if fn.endswith('ok') else 1
-                    with open(fn + '.msg') as msg:
-                        with open(fn + '.out') as out:
-                            out_text = out.read().replace(
-                                '%version%', cmv.__version__)
-                            dct['test_' + test] = create_test_method(
-                                msg.read(), out_text, exit_code,
-                                message_validator_name)
-        return super(MetaValidator, cls).__new__(cls, name, bases, dct)
+        for fn in os.listdir(specific_message_validator_test_path):
+            test, _, extension = fn.rpartition('.')
+            fn = os.path.join(specific_message_validator_test_path, test)
+            if extension == 'msg' and os.path.isfile(fn + '.out'):
+                exit_code = 0 if fn.endswith('ok') else 1
+                with open(fn + '.msg') as msg:
+                    with open(fn + '.out') as out:
+                        out_text = out.read().replace(
+                            '%version%', cmv.__version__)
+                        yield (
+                            msg.read(), out_text, exit_code,
+                            message_validator_name
+                        )
 
 
-class TestCommitMessageValidator(
-        with_metaclass(MetaValidator)):
-    """Validate the commit messages using test files."""
-    pass
+@pytest.mark.parametrize(
+    'msg,expected,expected_exit_code,message_validator_name',
+    generate_tests()
+)
+def test_validator(msg, expected, expected_exit_code,
+                   message_validator_name):
+    saved_stdout = sys.stdout
+    try:
+        out = StringIO()
+        sys.stdout = out
+        exit_code = cmv.check_message(
+            msg.splitlines(),
+            MESSAGE_VALIDATOR_MAP[message_validator_name])
+        # Ignore ANSI escapes in output
+        plain_out = RE_ESC.sub('', out.getvalue())
+        assert expected == plain_out
+        assert expected_exit_code == exit_code
+    finally:
+        sys.stdout = saved_stdout
