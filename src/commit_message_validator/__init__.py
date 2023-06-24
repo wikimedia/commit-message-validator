e74d3e564ffb2536e210e51c608b2b7eabeb3719
Bryan Davis <bd808@wikimedia.org>
2023-06-23 18:31:29 -0600

    Add header comment to all python files
    Add a standardized header comment marking each python file as
    a work of the Wikimedia Foundation and contributors that is licensed
    under the GPL-3.0-or-later license.

--- src/commit_message_validator/__init__.py
+++ src/commit_message_validator/__init__.py
@@ -1,24 +1,24 @@
-#!/usr/bin/env python3
+# Copyright (c) 2023 Wikimedia Foundation and contributors.
+# All Rights Reserved.
+#
+# This file is part of Commit Message Validator.
+#
+# Commit Message Validator is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Commit Message Validator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Commit Message Validator.  If not, see <http://www.gnu.org/licenses/>.
 """
 Validate the format of a commit message to Wikimedia Gerrit standards.
 
 https://www.mediawiki.org/wiki/Gerrit/Commit_message_guidelines
-
-Copyright (C) 2015 Bryan Davis and Wikimedia Foundation
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
 import os
--- tests/sample_repository.py
+++ tests/sample_repository.py
@@ -1,9 +1,23 @@
-#!/usr/bin/env python
-
+# Copyright (c) 2023 Wikimedia Foundation and contributors.
+# All Rights Reserved.
+#
+# This file is part of Commit Message Validator.
+#
+# Commit Message Validator is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# Commit Message Validator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Commit Message Validator.  If not, see <http://www.gnu.org/licenses/>.
 """
-Runs through the provided git repo and samples the commit
-messages to see whether they would pass the commit message
-validator.
+Runs through the provided git repo and samples the commit messages to see
+whether they would pass the commit message validator.
 """
 
 import os
